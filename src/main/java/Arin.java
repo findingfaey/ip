036f1f6497c7f78082836a9d7c30ce5640a8af10
findingfaey <yifeiyangyang@gmail.com>
2025-01-31 12:55:22 +0800

    A-Inheritance: Implement task types using inheritance

--- src/main/java/Arin.java
+++ src/main/java/Arin.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 class Task {
@@ -15,7 +14,7 @@ class Task {
     }
 
     public String getStatusIcon() {
-        return (isDone ? "[X]" : "[ ]"); // Return [X] if done, [ ] if not
+        return (isDone ? "[X]" : "[ ]");
     }
 
     @Override
@@ -66,9 +65,11 @@ class Event extends Task {
 }
 
 public class Arin {
+    private static Task[] tasks = new Task[100]; // Array of Tasks
+    private static int taskCount = 0; // Keep track of task count
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Task> tasks = new ArrayList<>();
 
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Arin.");
@@ -78,72 +79,111 @@ public class Arin {
         while (true) {
             String input = scanner.nextLine();
 
-            if (input.equals("bye")) {
-                System.out.println("____________________________________________________________");
-                System.out.println(" Bye. Hope to see you again soon!");
-                System.out.println("____________________________________________________________");
-                break;
-            } else if (input.equals("list")) {
-                System.out.println("____________________________________________________________");
-                System.out.println(" Here are the tasks in your list:");
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks.get(i));
-                }
-                System.out.println("____________________________________________________________");
-            } else if (input.startsWith("mark ")) {
-                try {
-                    int taskIndex = Integer.parseInt(input.substring(5)) - 1;
-                    if (taskIndex >= 0 && taskIndex < tasks.size()) {
-                        tasks.get(taskIndex).markAsDone();
-                        System.out.println("____________________________________________________________");
-                        System.out.println(" Nice! I've marked this task as done:");
-                        System.out.println("   " + tasks.get(taskIndex));
-                        System.out.println("____________________________________________________________");
-                    } else {
-                        System.out.println(" Task number out of range!");
-                    }
-                } catch (NumberFormatException e) {
-                    System.out.println(" Please enter a valid task number to mark!");
-                }
-            } else if (input.startsWith("todo ")) {
-                String taskDescription = input.substring(5).trim();
-                tasks.add(new ToDo(taskDescription));
-                System.out.println("____________________________________________________________");
-                System.out.println(" Got it. I've added this task:");
-                System.out.println("   " + tasks.get(tasks.size() - 1));
-                System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
-                System.out.println("____________________________________________________________");
-            } else if (input.startsWith("deadline ")) {
-                try {
-                    String[] parts = input.substring(9).split(" /by ");
-                    tasks.add(new Deadline(parts[0], parts[1]));
+            try {
+                if (input.equals("bye")) {
                     System.out.println("____________________________________________________________");
-                    System.out.println(" Got it. I've added this task:");
-                    System.out.println("   " + tasks.get(tasks.size() - 1));
-                    System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
+                    System.out.println(" Bye. Hope to see you again soon!");
                     System.out.println("____________________________________________________________");
-                } catch (Exception e) {
-                    System.out.println(" Invalid format! Use: deadline <task> /by <due date>");
+                    break;
+                } else if (input.equals("list")) {
+                    listTasks();
+                } else if (input.startsWith("mark ")) {
+                    markTaskAsDone(input);
+                } else if (input.startsWith("todo ")) {
+                    addToDo(input);
+                } else if (input.startsWith("deadline ")) {
+                    addDeadline(input);
+                } else if (input.startsWith("event ")) {
+                    addEvent(input);
+                } else {
+                    throw new Exception("OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
-            } else if (input.startsWith("event ")) {
-                try {
-                    String[] parts = input.substring(6).split(" /from | /to ");
-                    tasks.add(new Event(parts[0], parts[1], parts[2]));
-                    System.out.println("____________________________________________________________");
-                    System.out.println(" Got it. I've added this task:");
-                    System.out.println("   " + tasks.get(tasks.size() - 1));
-                    System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
-                    System.out.println("____________________________________________________________");
-                } catch (Exception e) {
-                    System.out.println(" Invalid format! Use: event <task> /from <start> /to <end>");
-                }
-            } else {
+            } catch (Exception e) {
                 System.out.println("____________________________________________________________");
-                System.out.println(" OOPS!!! I'm sorry, but I don't know what that means :-(");
+                System.out.println(" " + e.getMessage());
                 System.out.println("____________________________________________________________");
             }
         }
 
         scanner.close();
+    }
+
+    // 📝 List all tasks
+    private static void listTasks() {
+        System.out.println("____________________________________________________________");
+        if (taskCount == 0) {
+            System.out.println(" No tasks added yet!");
+        } else {
+            System.out.println(" Here are the tasks in your list:");
+            for (int i = 0; i < taskCount; i++) {
+                System.out.println(" " + (i + 1) + ". " + tasks[i]);
+            }
+        }
+        System.out.println("____________________________________________________________");
+    }
+
+    // ✅ Mark a task as done
+    private static void markTaskAsDone(String input) throws Exception {
+        try {
+            int taskIndex = Integer.parseInt(input.substring(5)) - 1;
+            if (taskIndex >= 0 && taskIndex < taskCount) {
+                tasks[taskIndex].markAsDone();
+                System.out.println("____________________________________________________________");
+                System.out.println(" Nice! I've marked this task as done:");
+                System.out.println("   " + tasks[taskIndex]);
+                System.out.println("____________________________________________________________");
+            } else {
+                throw new Exception("Task number out of range!");
+            }
+        } catch (NumberFormatException e) {
+            throw new Exception("Please enter a valid task number to mark!");
+        }
+    }
+
+    // ➕ Add a ToDo task
+    private static void addToDo(String input) throws Exception {
+        String taskDescription = input.replaceFirst("todo", "").trim();
+        if (taskDescription.isEmpty()) {
+            throw new Exception("The description of a todo cannot be empty.");
+        }
+        tasks[taskCount++] = new ToDo(taskDescription);
+        printTaskAdded();
+    }
+
+    // 📌 Add a Deadline task
+    private static void addDeadline(String input) throws Exception {
+        try {
+            String[] parts = input.substring(9).split(" /by ");
+            if (parts.length < 2) {
+                throw new Exception("Invalid format! Use: deadline <task> /by <due date>");
+            }
+            tasks[taskCount++] = new Deadline(parts[0], parts[1]);
+            printTaskAdded();
+        } catch (Exception e) {
+            throw new Exception("Invalid format! Use: deadline <task> /by <due date>");
+        }
+    }
+
+    // 🎉 Add an Event task
+    private static void addEvent(String input) throws Exception {
+        try {
+            String[] parts = input.substring(6).split(" /from | /to ");
+            if (parts.length < 3) {
+                throw new Exception("Invalid format! Use: event <task> /from <start> /to <end>");
+            }
+            tasks[taskCount++] = new Event(parts[0], parts[1], parts[2]);
+            printTaskAdded();
+        } catch (Exception e) {
+            throw new Exception("Invalid format! Use: event <task> /from <start> /to <end>");
+        }
+    }
+
+    // 📢 Print the last added task
+    private static void printTaskAdded() {
+        System.out.println("____________________________________________________________");
+        System.out.println(" Got it. I've added this task:");
+        System.out.println("   " + tasks[taskCount - 1]);
+        System.out.println(" Now you have " + taskCount + " tasks in the list.");
+        System.out.println("____________________________________________________________");
     }
 }
