16c487323dd6ec8f58584fd40e8a457c9c6e29e7
findingfaey <yifeiyangyang@gmail.com>
2025-01-31 12:41:03 +0800

    Level-2: Add support for adding and listing tasks

--- src/main/java/Arin.java
+++ src/main/java/Arin.java
@@ -1,41 +1,39 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Arin {
     public static void main(String[] args) {
-        // ASCII Logo
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<>();
 
-        // Greeting message
-        System.out.println("Hello from\n" + logo);
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Arin.");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
 
-        // Scanner to read user input
-        Scanner scanner = new Scanner(System.in);
-
         while (true) {
             String input = scanner.nextLine();
 
-            // Exit condition
-            if (input.equalsIgnoreCase("bye")) {
+            if (input.equals("bye")) {
                 System.out.println("____________________________________________________________");
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (input.equals("list")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Here are the tasks in your list:");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks.get(i));
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                tasks.add(input);
+                System.out.println("____________________________________________________________");
+                System.out.println(" added: " + input);
+                System.out.println("____________________________________________________________");
             }
-
-            // Echo user input
-            System.out.println("____________________________________________________________");
-            System.out.println(" " + input);
-            System.out.println("____________________________________________________________");
         }
 
-        scanner.close();  // Close scanner
+        scanner.close();
     }
 }
