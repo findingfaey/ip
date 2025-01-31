194fd336c2bc787826636b4fef0624b6075f94f0
findingfaey <yifeiyangyang@gmail.com>
2025-01-31 12:43:13 +0800

    Level-3: Add support for marking tasks as done

--- src/main/java/Arin.java
+++ src/main/java/Arin.java
@@ -1,10 +1,33 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "[X]" : "[ ]"); // Return [X] if done, [ ] if not
+    }
+
+    @Override
+    public String toString() {
+        return getStatusIcon() + " " + description;
+    }
+}
+
 public class Arin {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
 
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Arin.");
@@ -26,8 +49,23 @@ public class Arin {
                     System.out.println(" " + (i + 1) + ". " + tasks.get(i));
                 }
                 System.out.println("____________________________________________________________");
+            } else if (input.startsWith("mark ")) {
+                try {
+                    int taskIndex = Integer.parseInt(input.substring(5)) - 1;
+                    if (taskIndex >= 0 && taskIndex < tasks.size()) {
+                        tasks.get(taskIndex).markAsDone();
+                        System.out.println("____________________________________________________________");
+                        System.out.println(" Nice! I've marked this task as done:");
+                        System.out.println("   " + tasks.get(taskIndex));
+                        System.out.println("____________________________________________________________");
+                    } else {
+                        System.out.println(" Task number out of range!");
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println(" Please enter a valid task number to mark!");
+                }
             } else {
-                tasks.add(input);
+                tasks.add(new Task(input));
                 System.out.println("____________________________________________________________");
                 System.out.println(" added: " + input);
                 System.out.println("____________________________________________________________");
